9676ea2b62fa9bcee0a5dacc797d6aeac82ce706
Panha <laupanhavuth@gmail.com>
2022-04-05 02:33:54 -0400

    Check pickup availability Fixes #7

--- types/carrierpickupchange.go
+++ types/carrierpickupchange.go
@@ -21,3 +21,27 @@ type CarrierPickupChangeRequest struct {
 	ConfirmationNumber  string                  `xml:"ConfirmationNumber,omitempty"`
 	EmailAddress        string                  `xml:"EmailAddress,omitempty"`
 }
+
+type CarrierPickupChangeResponse struct {
+	FirstName           string                `xml:"FirstName"`
+	LastName            string                `xml:"LastName"`
+	FirmName            string                `xml:"FirmName,omitempty"`
+	SuiteOrApt          string                `xml:"SuiteOrApt,omitempty"`
+	Address2            string                `xml:"Address2"`
+	Urbanization        string                `xml:"Urbanization"`
+	City                string                `xml:"City"`
+	State               string                `xml:"State"`
+	ZIP5                string                `xml:"ZIP5"`
+	ZIP4                string                `xml:"ZIP4"`
+	Phone               string                `xml:"Phone"`
+	Extension           string                `xml:"Extension"`
+	Package             *CarrierPickupPackage `xml:"Package"`
+	EstimatedWeight     string                `xml:"EstimatedWeight"`
+	PackageLocation     string                `xml:"PackageLocation"`
+	SpecialInstructions string                `xml:"SpecialInstructions"`
+	ConfirmationNumber  string                `xml:"ConfirmationNumber,omitempty"`
+	DayOfWeek           string                `xml:"DayOfWeek"`
+	Date                string                `xml:"Date"`
+	Status              string                `xml:"Status,omitempty"`
+	EmailAddress        string                `xml:"EmailAddress,omitempty"`
+}
